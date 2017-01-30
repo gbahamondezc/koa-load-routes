f6ebee9f402a4a4b21eb0e291bb4e4cf67511cda
ijse <i@ijser.cn>
2017-01-31 06:20:27 +0800

    adding recursive option to support loading routes in sub directory

---
+++ test/routes/multiple/sub/subRoute.js
@@ -0,0 +1,17 @@
+module.exports = function () {
+
+  this.get('/sub/sub-route-gen', function*(next) {
+    yield next;
+    this.body = 'hello world from gen';
+  });
+
+  this.get('/sub/sub-route-fn', function(ctx, next) {
+    ctx.body = 'hello world from normal';
+    return next();
+  });
+
+  // this.get('/some', async (ctx, next) => {
+  //   await next();
+  // });
+
+};
